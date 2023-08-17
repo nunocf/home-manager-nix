113110eeb03a54502b6c4e2f651910d4f66d75d6
Nuno Ferreira <nunogcferreira@gmail.com>
2023-08-17 13:34:47 +0100

    nicer close icon in bufferline

--- nvim/lua/_bufferline.lua
+++ nvim/lua/_bufferline.lua
@@ -7,7 +7,7 @@ require "bufferline".setup {
     right_mouse_command = "bdelete! %d", -- can be a string | function, see "Mouse actions"
     left_mouse_command = "buffer %d",    -- can be a string | function, see "Mouse actions"
     middle_mouse_command = nil,          -- can be a string | function, see "Mouse actions"
-    buffer_close_icon = "",
+    buffer_close_icon = "",
     modified_icon = "●",
     close_icon = "",
     left_trunc_marker = "",
